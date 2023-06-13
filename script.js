8b0c8c15ea9af96d8889db479d8a5e5c792797f3
Awa1995 <128243812+Awa1995@users.noreply.github.com>
2023-06-13 16:11:35 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+/* Toggle between adding and removing the "responsive" class to navbar when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("navbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+/* Sélection des éléments HTML */
+let link = document.getElementById('navbar')
+let burger = document.getElementById('burger')
+let ul = document.querySelector('ul')
+
+/* gestionnaire d'événement sur le a#link pour venir changer l'attribution de la classe .open à la ul et au span#burger */
+link.addEventListener('click', function(e) {
+  e.preventDefault()
+  burger.classList.toggle('open')
+  ul.classList.toggle('open')
+})
